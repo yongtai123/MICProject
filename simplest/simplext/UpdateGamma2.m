eb9ee1c36545907ca0b969a9aa1893c24833af52
yongtai123 <34142715+yongtai123@users.noreply.github.com>
2017-12-12 16:57:01 -0600

    Delete UpdateGamma2.m

--- simplest/simplext/UpdateGamma2.m
+++
@@ -1,9 +0,0 @@
-function gamma2 = UpdateGamma2(numAgents, gamma2, Adjacency, w, phi, niu)
-
-    for k = 1:numAgents
-        for l = 1:numAgents
-            if Adjacency(l,k) == 1
-                gamma2(l,k) = (1-niu)*gamma2(l,k) + niu*(norm(w(:,k)-phi(:,l))^2);
-            end
-        end
-    end
